cfc6acfa0aa2e8f0e0dd3dc2ccc2a19b9d10f14a
https-sharif <sharif.islam96403@gmail.com>
2024-12-31 00:30:37 +0600

    fix: update SpeedInsights import path in header component

--- frontend/src/components/layout/header.tsx
+++ frontend/src/components/layout/header.tsx
@@ -6,7 +6,7 @@ import { useAuthStore } from "../../store/auth-store";
 import { useCartStore } from "../../store/cart-store";
 import { MobileNav } from "./mobile-navbar";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 export function Header() {
     const { isAuthenticated, user } = useAuthStore();
